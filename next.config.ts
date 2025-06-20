70b1f6b6142c1923fd66752b3e87832fbbbb51b2
mitulkanani <kmitul.dev@gmail.com>
2025-06-20 12:39:30 +0530

    https prblems

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/:path*',
+        destination: 'http://3.110.216.61/api/v1/:path*', // Proxy to backend
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
